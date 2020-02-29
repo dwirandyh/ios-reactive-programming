d53b4ca87f3b6398f033cc36a99c212aa6eb23df
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-02-29 21:57:10 +0700

    Rx Namer Project

---
+++ 4. Rx Namer/rxnamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxnamer
+//
+//  Created by Dwi Randy Herdinanto on 29/02/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
