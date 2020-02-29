a878002af2cd494196ed631c3a3effb7bc32a610
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-02-29 22:08:52 +0700

    Installing RxSwift & RxCocoa

---
+++ 4. Rx Namer/Pods/Target Support Files/Pods-rxnamer-rxnamerUITests/Pods-rxnamer-rxnamerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rxnamer_rxnamerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_rxnamer_rxnamerUITests
+@end
---
+++ 4. Rx Namer/Pods/Target Support Files/Pods-rxnamer/Pods-rxnamer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rxnamer : NSObject
+@end
+@implementation PodsDummy_Pods_rxnamer
+@end
