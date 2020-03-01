a50f2b027b14fb29950d7e1887ecfc0e9f2726f9
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-03-01 16:06:14 +0700

    Feature(Rx Namer): Add functionality

--- 4. Rx Namer/rxnamer/ViewController.swift
+++ 4. Rx Namer/rxnamer/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var helloLabel: UILabel!
     @IBOutlet weak var nameEntryTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var nameLabels: UILabel!
     
+    var namesArray : [String] = []
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bind()
+        bindSubmitButton()
     }
-
-
+    
+    func bind(){
+        nameEntryTextField.rx.text
+            .map { (result) in
+                if result == "" {
+                    return "Type your name below."
+                }else {
+                    return "Hello, \(result!)"
+                }
+        }.bind(to: helloLabel.rx.text).disposed(by: self.disposeBag)
+    }
+    
+    func bindSubmitButton(){
+        submitButton.rx.tap.subscribe(onNext: {
+            if self.nameEntryTextField.text != "" {
+                self.namesArray.append(self.nameEntryTextField.text!)
+                self.nameLabels.rx.text.onNext(self.namesArray.joined(separator: ", "))
+                self.nameEntryTextField.rx.text.onNext("")
+                self.helloLabel.rx.text.onNext("Type your name below.")
+            }
+        }).disposed(by: disposeBag)
+    }
 }
 
