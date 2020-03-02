9b86d75a1aacf80cae1ea576fddb3b7f672d1d1a
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-03-02 10:47:42 +0700

    feature(Github Trending): Building Repo Model Layer

---
+++ 5. Github Topic App/github_topic_app/Model/Repo.swift
@@ -0,0 +1,29 @@
+//
+//  Repo.swift
+//  github_topic_app
+//
+//  Created by Dwi Randy Herdinanto on 02/03/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class Repo {
+    public private(set) var image: UIImage
+    public private(set) var name: String
+    public private(set) var description: String
+    public private(set) var numberOfForks: Int
+    public private(set) var language: String
+    public private(set) var numberOfContributors: Int
+    public private(set) var repoUrl: String
+    
+    init(image: UIImage, name: String, description: String, numberOfForks: Int, language: String, numberOfContributors: Int, repoUrl: String) {
+        self.image = image
+        self.name = name
+        self.description = description
+        self.numberOfForks = numberOfForks
+        self.language = language
+        self.numberOfContributors = numberOfContributors
+        self.repoUrl = repoUrl
+    }
+}
